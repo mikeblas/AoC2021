c288607c496a0e0eb614d1c1b1284404b787dff8
Mike Blaszczak <mikeblas@gmail.com>
2021-12-08 22:04:51 -0800

    Day 9 part 2

--- Day09/Day09.py
+++ Day09/Day09.py
@@ -1,7 +1,7 @@
 
 import pprint
 
-with open('sample.txt') as my_file:
+with open('input.txt') as my_file:
     input_lines = my_file.readlines()
 input_lines = [s.strip() for s in input_lines]
 line_count = len(input_lines)
@@ -36,7 +36,7 @@ for y in range(0, map_height):
         low_points += 1
         low_costs += 1 + this_one
         print(f"at {x},{y} found {this_one}")
-        low_point_list.append((x,y))
+        low_point_list.append((x, y))
 
 # 1718 incorrect
 print(f"{low_points} low points")
@@ -44,4 +44,47 @@ print(f"{low_costs} low costs")
 
 pprint.pprint(low_point_list)
 
-# ---- part 2
+# ---- part
+
+pond_sizes = []
+while len(low_point_list) > 0:
+
+    (start_x, start_y) = low_point_list.pop()
+    visit_queue = []
+    visit_queue.append((start_x, start_y))
+    print(f"starting {start_x},{start_y}")
+    visited = [[False for j in range(map_width)] for i in range(map_height)]
+    # pprint.pprint(visited)
+    pond_size = 0
+
+    while len(visit_queue) > 0:
+        (current_x, current_y) = visit_queue.pop()
+
+        # print(f"working {current_x}, {current_y}, len = {len(visit_queue)}")
+        if visited[current_y][current_x]:
+            continue
+        if the_map[current_y][current_x] == 9:
+            continue
+
+        visited[current_y][current_x] = True
+        pond_size += 1
+        if current_y-1 >= 0:
+            visit_queue.append((current_x, current_y-1))
+        if current_y+1 < map_height:
+            visit_queue.append((current_x, current_y+1))
+        if current_x-1 >= 0:
+            visit_queue.append((current_x-1, current_y))
+        if current_x+1 < map_width:
+            visit_queue.append((current_x+1, current_y))
+
+    print(f"pond size is {pond_size}")
+    pond_sizes.append(pond_size)
+
+pond_sizes.sort(reverse=True)
+total_product = 1
+for top in range(0, 3):
+    print(f"size {top} is {pond_sizes[top]}")
+    total_product *= pond_sizes[top]
+
+print(f"total product is {total_product}")
+
