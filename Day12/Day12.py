192b12839205d5b9859fd2d74f5030e39878e2b1
Mike Blaszczak <mikeblas@gmail.com>
2021-12-12 06:04:18 -0800

    progress on Day 12 part 2

--- Day12/Day12.py
+++ Day12/Day12.py
@@ -1,6 +1,4 @@
-
 import pprint
-
 
 class Graph:
 
@@ -39,9 +37,62 @@ class Graph:
         return self.paths
 
 
+    def get_histogram(self, visited):
+        histogram = {}
+        for place in visited:
+            if self.reentrant(place):
+                continue
+            if place not in histogram:
+                histogram[place] = 1
+            else:
+                histogram[place] += 1
+        return histogram
 
 
-with open('input.txt') as my_file:
+    def can_enter(self, visited, target):
+
+        # can't enter start
+        if target == 'start':
+            return False
+
+        # if reentrant, we can always visit
+        if self.reentrant(target):
+            return True
+
+        # if never entered, we can visit
+        if target not in visited:
+            return True
+
+        # target was visited before, but we must be sure no other was visited before
+        histogram = self.get_histogram(visited)
+        if not any(v >= 2 for v in iter(histogram.values())):
+            return True
+
+        # otherwise, no
+        return False
+
+    def more_traversals2(self, place, place_paths, visited):
+
+        if place == "end":
+            self.paths.append(visited + ["end"])
+        else:
+            for next_place in place_paths:
+                if self.can_enter(visited, next_place):
+                    self.more_traversals2(next_place, self.points[next_place], visited + [place])
+
+
+    def all_traversals2(self):
+
+        self.paths.clear()
+        visited = []
+
+        self.more_traversals2("start", self.points["start"], [])
+
+        return self.paths
+
+
+
+with open('sample2.txt') as my_file:
     input_lines = my_file.readlines()
 input_lines = [s.strip() for s in input_lines]
 line_count = len(input_lines)
@@ -53,10 +104,16 @@ the_graph = Graph()
 
 for line in input_lines:
     fields = line.split("-")
-    print(f"{fields[0]} to {fields[1]}")
+    # print(f"{fields[0]} to {fields[1]}")
     the_graph.add_point(fields[0], fields[1])
 
 the_graph.all_traversals()
 
-print(f"{the_graph.paths}")
+# print(f"{the_graph.paths}")
+print(f"{len(the_graph.paths)}")
+
+
+# 195155 is too high
+the_graph.all_traversals2()
+pprint.pprint(the_graph.paths)
 print(f"{len(the_graph.paths)}")
