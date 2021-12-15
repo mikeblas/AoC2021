f06dc0d5aa4046fdecb728ffb37583081c224d95
Mike Blaszczak <mikeblas@gmail.com>
2021-12-15 11:57:24 -0800

    Day 15 Part 1 cleanup

--- Day15/Day15.py
+++ Day15/Day15.py
@@ -1,18 +1,6 @@
 import heapq
 import sys
 import pprint
-from queue import PriorityQueue
-
-with open('input.txt') as my_file:
-    input_lines = my_file.readlines()
-input_lines = [s.strip() for s in input_lines]
-line_count = len(input_lines)
-print(f"{line_count} lines read")
-
-cells = [[int(x) for x in line] for line in input_lines]
-print(f"width is {len(cells[0])}")
-print(f"height  is {len(cells)}")
-
 
 class CellChoice:
     def __init__(self, x, y, distance):
@@ -33,8 +21,7 @@ class CellChoice:
             return self.distance < other.distance
 
 
-
-def min_cost3(cols, rows):
+def min_cost3(cells, cols, rows):
 
     tc = [[sys.maxsize for x in range(cols)] for y in range(rows)]
     tc[0][0] = 0
@@ -74,5 +61,19 @@ def min_cost3(cols, rows):
     return tc[rows-1][cols-1]
 
 
+def main():
+    with open('input.txt') as my_file:
+        input_lines = my_file.readlines()
+    input_lines = [s.strip() for s in input_lines]
+    line_count = len(input_lines)
+    print(f"{line_count} lines read")
 
-print(min_cost3(len(cells[0]), len(cells)))
+    cells = [[int(x) for x in line] for line in input_lines]
+    print(f"width is {len(cells[0])}")
+    print(f"height  is {len(cells)}")
+
+    print(min_cost3(cells, len(cells[0]), len(cells)))
+
+
+if __name__ == '__main__':
+    main()
